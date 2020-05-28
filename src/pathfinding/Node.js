88bfa27e3b67be252322180b09e183c3a8a00edd
De Wulf Jerome <dewulje@cronos.be>
2020-05-28 21:35:41 +0200

    styled components cleanup

--- src/pathfinding/Node.js
+++ src/pathfinding/Node.js
@@ -8,59 +8,53 @@ const StyledNode = styled.div`
   outline: 1px solid grey;
   display: inline-block;
   background: #f2f2f2;
+  :hover {
+    cursor: pointer;
+  }
 `;
 
-const StartNode = styled.div`
-  height: 100%;
-  width: 100%;
-  outline: 1px solid grey;
-  display: inline-block;
+const StartNode = styled(StyledNode)`
   background: green;
 `;
 
-const FinishNode = styled.div`
-  height: 100%;
-  width: 100%;
-  outline: 1px solid grey;
-  display: inline-block;
+const FinishNode = styled(StyledNode)`
   background: red;
 `;
 
-export default function Node({ nodeData, onClick }) {
-  const extraClassName = nodeData.isFinish
-    ? 'node-finish'
-    : nodeData.isStart
-    ? 'node-start'
-    : nodeData.isWall
-    ? 'node-wall'
-    : '';
+const WallNode = styled(StyledNode)`
+  background: rgb(12, 53, 71);
+`;
 
+export default function Node({ nodeData, onClick }) {
   const clickedNode = () => {
     onClick(nodeData.col, nodeData.row);
   };
 
   if (nodeData.isStart) {
-    return (
-      <StartNode
-        className={`node ${extraClassName}`}
-        id={`node-${nodeData.row}-${nodeData.col}`}
-        onClick={clickedNode}
-      ></StartNode>
-    );
+    return <StartNode id={`node-${nodeData.row}-${nodeData.col}`}></StartNode>;
   } else if (nodeData.isFinish) {
     return (
-      <FinishNode
-        className={`node ${extraClassName}`}
+      <FinishNode id={`node-${nodeData.row}-${nodeData.col}`}></FinishNode>
+    );
+  } else if (nodeData.isWall) {
+    return (
+      <WallNode
         id={`node-${nodeData.row}-${nodeData.col}`}
-        onClick={clickedNode}
-      ></FinishNode>
+        onMouseDown={(e) => {
+          e.preventDefault();
+          clickedNode();
+        }}
+        onMouseEnter={(e) => {
+          if (e.buttons === 1) {
+            clickedNode();
+          }
+        }}
+      ></WallNode>
     );
   } else {
     return (
       <StyledNode
-        className={`node ${extraClassName}`}
         id={`node-${nodeData.row}-${nodeData.col}`}
-        // onClick={clickedNode}
         onMouseDown={(e) => {
           e.preventDefault();
           clickedNode();
