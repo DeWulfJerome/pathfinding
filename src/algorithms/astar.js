a5bb3396ddd404977a3f9653ec8a435824702d0e
De Wulf Jerome <dewulje@cronos.be>
2020-07-20 00:41:41 +0200

    astar extends dijkstra

--- src/algorithms/astar.js
+++ src/algorithms/astar.js
@@ -3,12 +3,11 @@ import {
   horvertEdgeDistance
 } from '../dataStructures/graph';
 import * as _ from 'lodash';
+import Dijkstra from './dijkstra';
 
-export default class Astar {
+export default class Astar extends Dijkstra {
   constructor(graph, startNode, finishNode) {
-    this.graph = graph;
-    this.startNode = startNode;
-    this.finishNode = finishNode;
+    super(graph, startNode, finishNode);
   }
 
   getPreviousNodesAndVisitedNodesInOrder() {
@@ -42,41 +41,13 @@ export default class Astar {
         continue;
       }
 
-      // For this node, check its unvisited neighbours
-      currentNode[1].neighbours.forEach((neighbour) => {
-        // For this node, calculate distance of each neighbour from the start node
-        if (unvisitedNodes.has(neighbour.name)) {
-          const tempNode = unvisitedNodes.get(neighbour.name);
-          if (distanceToNodes.has(currentNode[0])) {
-            tempNode.distance =
-              distanceToNodes.get(currentNode[0]) +
-              neighbour.distanceToNeighbour;
-          } else {
-            // Is startnode
-            tempNode.distance = neighbour.distanceToNeighbour;
-          }
-          unvisitedNodes.get(neighbour.name);
-          if (distanceToNodes.has(neighbour.name)) {
-            if (distanceToNodes.get(neighbour.name) > tempNode.distance) {
-              // If the calc distance is less than the known distance, update the shortest path
-              distanceToNodes.set(neighbour.name, tempNode.distance);
-              // Update the previous node for each of the updated distances
-              previousNodes.set(neighbour.name, currentNode[0]);
-            } else {
-              // Reset the distance in the unvisited node to the closest known distance
-              tempNode.distance = distanceToNodes.get(neighbour.name);
-            }
-          } else {
-            distanceToNodes.set(neighbour.name, tempNode.distance);
-            // Update the previous node for each of the updated distances
-            previousNodes.set(neighbour.name, currentNode[0]);
-          }
-        }
-      });
-      // Push the current node name into visitedNodesInOrder to be able to show the algorithm working
-      visitedNodesInOrder.push(currentNode[0]);
-      // Add the current node to the list of visited nodes / remove from unvisited nodes
-      unvisitedNodes.delete(currentNode[0]);
+      this.checkNeighbours(
+        currentNode,
+        unvisitedNodes,
+        previousNodes,
+        visitedNodesInOrder,
+        distanceToNodes
+      );
     }
 
     return {
@@ -142,18 +113,5 @@ export default class Astar {
         );
       })
       .shift();
-  }
-
-  findShortestPath(previousNodes) {
-    const nodesInReverseOrder = [this.finishNode];
-    let lookupNode = this.finishNode;
-    while (true) {
-      nodesInReverseOrder.push(previousNodes.get(lookupNode));
-      lookupNode = previousNodes.get(lookupNode);
-      if (lookupNode === this.startNode) {
-        break;
-      }
-    }
-    return nodesInReverseOrder.reverse();
   }
 }
--- src/algorithms/dijkstra.js
+++ src/algorithms/dijkstra.js
@@ -15,6 +15,7 @@ export default class Dijkstra {
     const previousNodes = new Map();
     const distanceToNodes = new Map();
     const visitedNodesInOrder = [];
+
     while (unvisitedNodes.size) {
       // Visit the node with the smallest known distance from the start node
       const currentNode = this.getClosestNode(unvisitedNodes);
@@ -30,46 +31,61 @@ export default class Dijkstra {
         unvisitedNodes.delete(currentNode[0]);
         continue;
       }
-      // For this node, check its unvisited neighbours
-      currentNode[1].neighbours.forEach((neighbour) => {
-        // For this node, calculate distance of each neighbour from the start node
-        if (unvisitedNodes.has(neighbour.name)) {
-          const tempNode = unvisitedNodes.get(neighbour.name);
-          if (distanceToNodes.has(currentNode[0])) {
-            tempNode.distance =
-              distanceToNodes.get(currentNode[0]) +
-              neighbour.distanceToNeighbour;
-          } else {
-            // Is startnode
-            tempNode.distance = neighbour.distanceToNeighbour;
-          }
-          unvisitedNodes.get(neighbour.name);
-          if (distanceToNodes.has(neighbour.name)) {
-            if (distanceToNodes.get(neighbour.name) > tempNode.distance) {
-              // If the calc distance is less than the known distance, update the shortest path
-              distanceToNodes.set(neighbour.name, tempNode.distance);
-              // Update the previous node for each of the updated distances
-              previousNodes.set(neighbour.name, currentNode[0]);
-            } else {
-              // Reset the distance in the unvisited node to the closest known distance
-              tempNode.distance = distanceToNodes.get(neighbour.name);
-            }
-          } else {
-            distanceToNodes.set(neighbour.name, tempNode.distance);
-            // Update the previous node for each of the updated distances
-            previousNodes.set(neighbour.name, currentNode[0]);
-          }
-        }
-      });
-      // Push the current node name into visitedNodesInOrder to be able to show the algorithm working
-      visitedNodesInOrder.push(currentNode[0]);
-      // Add the current node to the list of visited nodes / remove from unvisited nodes
-      unvisitedNodes.delete(currentNode[0]);
+      this.checkNeighbours(
+        currentNode,
+        unvisitedNodes,
+        previousNodes,
+        visitedNodesInOrder,
+        distanceToNodes
+      );
     }
     return {
       previousNodes,
       visitedNodesInOrder
     };
+  }
+
+  checkNeighbours(
+    currentNode,
+    unvisitedNodes,
+    previousNodes,
+    visitedNodesInOrder,
+    distanceToNodes
+  ) {
+    // For this node, check its unvisited neighbours
+    currentNode[1].neighbours.forEach((neighbour) => {
+      // For this node, calculate distance of each neighbour from the start node
+      if (unvisitedNodes.has(neighbour.name)) {
+        const tempNode = unvisitedNodes.get(neighbour.name);
+        if (distanceToNodes.has(currentNode[0])) {
+          tempNode.distance =
+            distanceToNodes.get(currentNode[0]) + neighbour.distanceToNeighbour;
+        } else {
+          // Is startnode
+          tempNode.distance = neighbour.distanceToNeighbour;
+        }
+        unvisitedNodes.get(neighbour.name);
+        if (distanceToNodes.has(neighbour.name)) {
+          if (distanceToNodes.get(neighbour.name) > tempNode.distance) {
+            // If the calc distance is less than the known distance, update the shortest path
+            distanceToNodes.set(neighbour.name, tempNode.distance);
+            // Update the previous node for each of the updated distances
+            previousNodes.set(neighbour.name, currentNode[0]);
+          } else {
+            // Reset the distance in the unvisited node to the closest known distance
+            tempNode.distance = distanceToNodes.get(neighbour.name);
+          }
+        } else {
+          distanceToNodes.set(neighbour.name, tempNode.distance);
+          // Update the previous node for each of the updated distances
+          previousNodes.set(neighbour.name, currentNode[0]);
+        }
+      }
+    });
+    // Push the current node name into visitedNodesInOrder to be able to show the algorithm working
+    visitedNodesInOrder.push(currentNode[0]);
+    // Add the current node to the list of visited nodes / remove from unvisited nodes
+    unvisitedNodes.delete(currentNode[0]);
   }
 
   getClosestNode(unvisitedNodes) {
